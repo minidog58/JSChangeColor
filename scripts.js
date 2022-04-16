541e34bd62ef9982286ee631c75f064a41ad4ce2
phong <phong.tran58@hotmail.com>
2022-04-15 20:13:03 -0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function fontColorRed(){
+   document.getElementById("str").style.color = "red";
+}
+function fontColorBlue(){
+    document.getElementById("str").style.color = "blue";
+ }
+ function fontColorGreen(){
+    document.getElementById("str").style.color = "green";
+ }
+ function boxColor(color){
+    document.getElementById("box2").style.backgroundColor = color;
+}
